2d13c7f8a851bb6d951276a9c793384e5f995ad9
Ihar Bury <Ihar.Bury@gmail.com>
2016-05-01 11:56:39 +0200

    Transformed to 4.6.1

--- src/IharBury.Algorithms/Properties/AssemblyInfo.cs
+++ src/IharBury.Algorithms/Properties/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IharBury.Algorithms")]
 [assembly: AssemblyProduct("IharBury.Algorithms")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
