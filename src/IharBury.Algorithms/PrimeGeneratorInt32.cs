cb521a123a08bfb05fce5fe5ddb30481b851eb0c
Ihar Bury <Ihar.Bury@gmail.com>
2016-05-01 21:56:01 +0200

    PrimeGeneratorInt32 added (#4)

---
+++ src/IharBury.Algorithms/PrimeGeneratorInt32.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IharBury.Algorithms
+{
+    public sealed class PrimeGeneratorInt32 : IEnumerable<int>
+    {
+        private List<int> primes = new List<int> { 2, 3 };
+        private bool areAllPrimesFound;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            var primeIndex = 0;
+            while (true)
+            {
+                if (primeIndex == primes.Count)
+                {
+                    if (!TryFindNextPrime())
+                        yield break;
+                }
+
+                yield return primes[primeIndex];
+                primeIndex++;
+            }
+        }
+
+        private bool TryFindNextPrime()
+        {
+            if (areAllPrimesFound)
+                return false;
+
+            var candidate = primes[primes.Count - 1];
+            while (candidate <= int.MaxValue - 2)
+            {
+                candidate += 2;
+                if (IsCandidatePrime(candidate))
+                {
+                    primes.Add(candidate);
+                    return true;
+                }
+            }
+
+            areAllPrimesFound = true;
+            return false;
+        }
+
+        private bool IsCandidatePrime(int candidate)
+        {
+            foreach (var prime in primes)
+            {
+                if (candidate / prime < prime)
+                    break;
+                if (candidate % prime == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/PrimeGeneratorInt32Tests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace IharBury.Algorithms.Tests
+{
+    public class PrimeGeneratorInt32Tests
+    {
+        [Fact]
+        public void FirstInt32PrimesCanBeEnumerated()
+        {
+            var primes = new PrimeGeneratorInt32().Take(7).ToList();
+            Assert.Equal(2, primes[0]);
+            Assert.Equal(3, primes[1]);
+            Assert.Equal(5, primes[2]);
+            Assert.Equal(7, primes[3]);
+            Assert.Equal(11, primes[4]);
+            Assert.Equal(13, primes[5]);
+            Assert.Equal(17, primes[6]);
+        }
+    }
+}
