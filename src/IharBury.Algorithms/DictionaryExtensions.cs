45ebf0dd56cce8105ff02383495305ef14916ecb
Ihar Bury <Ihar.Bury@gmail.com>
2016-05-01 15:36:57 +0200

    Dictionary extensions alternative to TryGet (#2)
    GetValueOrDefault, GetValueOrDefaultForReadOnly,
    GetNullableValueOrNull,
    GetNullableValueOrNullForReadOnly extension methods added for dictionaries to avoid using TryGet

---
+++ src/IharBury.Algorithms/DictionaryExtensions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IharBury.Algorithms
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue defaultValue = default(TValue))
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            TValue result;
+            return dictionary.TryGetValue(key, out result) ? result : defaultValue;
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IReadOnlyDictionary<TKey, TValue> dictionary, 
+            TKey key,
+            TValue defaultValue = default(TValue))
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            return dictionary.GetValueOrDefaultForReadOnly(key, defaultValue);
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this Dictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue defaultValue = default(TValue))
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            return dictionary.GetValueOrDefaultForReadOnly(key, defaultValue);
+        }
+
+        public static TValue GetValueOrDefaultForReadOnly<TKey, TValue>(
+            this IReadOnlyDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue defaultValue = default(TValue))
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            TValue result;
+            return dictionary.TryGetValue(key, out result) ? result : defaultValue;
+        }
+
+        public static TValue? GetNullableValueOrNull<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : struct
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            TValue result;
+            return dictionary.TryGetValue(key, out result) ? result : (TValue?)null;
+        }
+
+        public static TValue? GetNullableValueOrNull<TKey, TValue>(
+            this IReadOnlyDictionary<TKey, TValue> dictionary, 
+            TKey key)
+            where TValue : struct
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            return dictionary.GetNullableValueOrNullForReadOnly(key);
+        }
+
+        public static TValue? GetNullableValueOrNull<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : struct
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            return dictionary.GetNullableValueOrNullForReadOnly(key);
+        }
+
+        public static TValue? GetNullableValueOrNullForReadOnly<TKey, TValue>(
+            this IReadOnlyDictionary<TKey, TValue> dictionary,
+            TKey key)
+            where TValue : struct
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException(nameof(dictionary));
+
+            TValue result;
+            return dictionary.TryGetValue(key, out result) ? result : (TValue?)null;
+        }
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/DictionaryExtensionsTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace IharBury.Algorithms.Tests
+{
+    public class DictionaryExtensionsTests
+    {
+        public class WhenGettingValueOrDefault
+        {
+            [Fact]
+            public void DictionaryClassCanBeUsed()
+            {
+                new Dictionary<int, int>().GetValueOrDefault(1);
+            }
+
+            [Fact]
+            public void DictionaryInterfaceCanBeUsed()
+            {
+                IDictionary<int, int> dictionary = new Dictionary<int, int>();
+                dictionary.GetValueOrDefault(1);
+            }
+
+            [Fact]
+            public void ReadOnlyDictionaryInterfaceCanBeUsed()
+            {
+                IReadOnlyDictionary<int, int> dictionary = new Dictionary<int, int>();
+                dictionary.GetValueOrDefault(1);
+            }
+
+            [Fact]
+            public void WhenDictionaryContainsKeyItReturnsValue()
+            {
+                Assert.Equal(2, new Dictionary<int, int> { { 1, 2 } }.GetValueOrDefault(1));
+            }
+
+            [Fact]
+            public void WhenDictionaryDoesNotContainKeyItReturnsDefaultValue()
+            {
+                Assert.Equal(default(int), new Dictionary<int, int>().GetValueOrDefault(1));
+            }
+
+            [Fact]
+            public void WhenDictionaryDoesNotContainKeyAndDefaultValueGivenItReturnsTheGivenDefaultValue()
+            {
+                Assert.Equal(5, new Dictionary<int, int>().GetValueOrDefault(1, 5));
+            }
+        }
+
+        public class WhenGettingNullableValueOrNull
+        {
+            [Fact]
+            public void DictionaryClassCanBeUsed()
+            {
+                new Dictionary<int, int>().GetNullableValueOrNull(1);
+            }
+
+            [Fact]
+            public void DictionaryInterfaceCanBeUsed()
+            {
+                IDictionary<int, int> dictionary = new Dictionary<int, int>();
+                dictionary.GetNullableValueOrNull(1);
+            }
+
+            [Fact]
+            public void ReadOnlyDictionaryInterfaceCanBeUsed()
+            {
+                IReadOnlyDictionary<int, int> dictionary = new Dictionary<int, int>();
+                dictionary.GetNullableValueOrNull(1);
+            }
+
+            [Fact]
+            public void WhenDictionaryContainsKeyItReturnsValue()
+            {
+                Assert.Equal(2, new Dictionary<int, int> { { 1, 2 } }.GetNullableValueOrNull(1));
+            }
+
+            [Fact]
+            public void WhenDictionaryDoesNotContainKeyItReturnsDefaultValue()
+            {
+                Assert.Null(new Dictionary<int, int>().GetNullableValueOrNull(1));
+            }
+        }
+    }
+}
