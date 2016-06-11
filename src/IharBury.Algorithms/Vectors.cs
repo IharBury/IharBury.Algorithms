fa28db968d24486ff40d4c4b65357bb2f261077d
Ihar Bury <Ihar.Bury@gmail.com>
2016-06-11 15:40:08 +0200

    Vectors.IsSubvectorAt

---
+++ src/IharBury.Algorithms/Vectors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IharBury.Algorithms
+{
+    public static class Vectors
+    {
+        public static bool IsSubvectorAt<T>(
+            long vectorLength,
+            Func<long, T> getVectorItem,
+            long subvectorLength,
+            Func<long, T> getSubvectorItem,
+            long subvectorStartIndex,
+            IEqualityComparer<T> equalityComparer = null)
+        {
+            if (vectorLength < 0)
+                throw new ArgumentOutOfRangeException(nameof(vectorLength));
+            if (getVectorItem == null)
+                throw new ArgumentNullException(nameof(getVectorItem));
+            if (subvectorLength < 0)
+                throw new ArgumentOutOfRangeException(nameof(vectorLength));
+            if (getSubvectorItem == null)
+                throw new ArgumentNullException(nameof(getSubvectorItem));
+            if ((subvectorStartIndex < 0) || (subvectorStartIndex >= vectorLength))
+                throw new ArgumentOutOfRangeException(nameof(subvectorStartIndex));
+
+            if (checked(vectorLength - subvectorLength) < subvectorStartIndex)
+                return false;
+
+            var effectiveEqualityComparer = equalityComparer ?? EqualityComparer<T>.Default;
+            var subvectorIndex = 0L;
+            var vectorIndex = subvectorStartIndex;
+            while (subvectorIndex < subvectorLength)
+            {
+                if (!effectiveEqualityComparer.Equals(getVectorItem(vectorIndex), getSubvectorItem(subvectorIndex)))
+                    return false;
+
+                vectorIndex++;
+                subvectorIndex++;
+            }
+
+            return true;
+        }
+    }
+}
