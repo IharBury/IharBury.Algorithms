9cbb4829b53dd7c119718c713bba083ed7de0bd1
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-18 19:27:14 +0100

    Optional structure improvements

--- src/IharBury.Algorithms/Optional.cs
+++ src/IharBury.Algorithms/Optional.cs
@@ -1,37 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IharBury.Algorithms
 {
-    public struct Optional<T>
+    public static class Optional
+    {
+        /// <summary>
+        /// Constructs <see cref="Optional{T}"/> representing the given value.
+        /// </summary>
+        /// <param name="value">The value to be represented.</param>
+        public static Optional<T> From<T>(T value) => new Optional<T>(value);
+    }
+
+    /// <summary>
+    /// Represents a value of type <typeparamref name="T"/> that can be missing.
+    /// A missing value is still strongly typed and is not represented as <c>null</c>.
+    /// </summary>
+    /// <typeparam name="T">Type of the value when it is present.</typeparam>
+    public struct Optional<T> : IEquatable<Optional<T>>
     {
         public static implicit operator Optional<T>(T value)
         {
             return new Optional<T>(value);
         }
 
+        private static readonly IEqualityComparer<T> valueEqualityComparer = EqualityComparer<T>.Default;
+
+        /// <summary>
+        /// Represents the missing value.
+        /// </summary>
+        public static Optional<T> None => new Optional<T>();
+
         private readonly T value;
 
+        /// <summary>
+        /// Constructs <see cref="Optional{T}"/> representing the given value.
+        /// </summary>
+        /// <param name="value">The value to be represented.</param>
         public Optional(T value)
         {
             HasValue = true;
             this.value = value;
         }
 
+        /// <summary>
+        /// Whether the value is present.
+        /// </summary>
         public bool HasValue { get; }
+
+        /// <summary>
+        /// The represented value when it is present.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">When the value is missing.</exception>
         public T Value
         {
             get
             {
                 if (!HasValue)
                     throw new InvalidOperationException("There is no value.");
+
                 return value;
             }
         }
 
-        public override string ToString() => HasValue ? $"{Value}" : $"Empty {typeof(T)}";
+        public override string ToString() => HasValue ? $"{Value}" : $"Empty {typeof(Optional<T>)}";
 
-        public T OrDefault(T defaultValue) => HasValue ? Value : defaultValue;
+        /// <summary>
+        /// Returns the represented value or the given default value when the value is missing.
+        /// </summary>
+        /// <param name="defaultValue">The default value.</param>
+        public T OrDefault(T defaultValue = default(T)) => HasValue ? Value : defaultValue;
 
+        /// <summary>
+        /// Returns the represented value or a default value when the value is missing.
+        /// </summary>
+        /// <param name="getDefaultValue">
+        /// The delegate that should return the default value.
+        /// Cannot be <c>null</c>.
+        /// Is not invoked when the value is present.
+        /// </param>
+        /// <returns></returns>
         public T OrDefault(Func<T> getDefaultValue)
         {
             if (getDefaultValue == null)
@@ -39,5 +87,26 @@ namespace IharBury.Algorithms
 
             return HasValue ? Value : getDefaultValue();
         }
+
+        /// <summary>
+        /// Two optional values are considered equal when they represent the same present value
+        /// or when they both represent a missing value.
+        /// </summary>
+        public bool Equals(Optional<T> other) =>
+            HasValue ? other.HasValue && valueEqualityComparer.Equals(value, other.value) : !other.HasValue;
+
+        /// <summary>
+        /// Two optional values are considered equal when they represent the same present value
+        /// or when they both represent a missing value.
+        /// An optional value is not considered equal to the value it represents as the type is different.
+        /// </summary>
+        public override bool Equals(object obj) => (obj is Optional<T> other) && Equals(other);
+
+        /// <summary>
+        /// Two optional values are considered equal when they represent the same present value
+        /// or when they both represent a missing value.
+        /// </summary>
+        public override int GetHashCode() =>
+            HasValue ? unchecked(88_657 * (value == null ? 104_173 : valueEqualityComparer.GetHashCode(value))) : 103_043;
     }
 }
---
+++ test/IharBury.Algorithms.Tests/OptionalTests.cs
@@ -0,0 +1,313 @@
+﻿using FakeItEasy;
+using System;
+using Xunit;
+
+namespace IharBury.Algorithms.Tests
+{
+    public sealed class OptionalTests
+    {
+        [Fact]
+        public void DefaultHasNoValue()
+        {
+            Assert.False(default(Optional<int>).HasValue);
+        }
+
+        [Fact]
+        public void ConstructedWithValueHasTheValue()
+        {
+            var optional = new Optional<int>(1);
+            Assert.True(optional.HasValue);
+            Assert.Equal(1, optional.Value);
+        }
+
+        [Fact]
+        public void ConstructedWithNullValueHasNullValue()
+        {
+            var optional = new Optional<string>(null);
+            Assert.True(optional.HasValue);
+            Assert.Null(optional.Value);
+        }
+
+        [Fact]
+        public void ConstructedWithEmptyNullableValueHasTheValue()
+        {
+            var optional = new Optional<int?>(default(int?));
+            Assert.True(optional.HasValue);
+            Assert.Equal(default(int?), optional.Value);
+        }
+
+        [Fact]
+        public void ImplicitlyConvertedFromValueHasTheValue()
+        {
+            Optional<int> optional = 1;
+            Assert.True(optional.HasValue);
+            Assert.Equal(1, optional.Value);
+        }
+
+        [Fact]
+        public void ConstructedWithValueViaStaticMethodHasTheValue()
+        {
+            var optional = Optional.From(1);
+            Assert.True(optional.HasValue);
+            Assert.Equal(1, optional.Value);
+        }
+
+        [Fact]
+        public void AttemptToGetValueThrowsWhenThereIsNoValue()
+        {
+            var optional = default(Optional<int>);
+            Assert.Throws<InvalidOperationException>(() => optional.Value);
+        }
+
+        [Fact]
+        public void NoneHasNoValue()
+        {
+            Assert.False(Optional<int>.None.HasValue);
+        }
+
+        [Fact]
+        public void WithValueItIsConvertedToStringAsTheValue()
+        {
+            var optional = new Optional<int>(1);
+            Assert.Equal(1.ToString(), optional.ToString());
+        }
+
+        [Fact]
+        public void WithoutValueItIsConvertedToStringAsSpecialTextWithTheValueTypeName()
+        {
+            var optional = new Optional<int>();
+            Assert.Equal($"Empty {typeof(Optional<int>)}", optional.ToString());
+        }
+
+        public sealed class WhenGettingValueOrDefaultValueFromAConstant
+        {
+            [Fact]
+            public void WithValueItReturnsTheValue()
+            {
+                var optional = new Optional<int>(1);
+                Assert.Equal(1, optional.OrDefault(2));
+            }
+
+            [Fact]
+            public void WithoutValueItReturnsTheDefaultValue()
+            {
+                var optional = new Optional<int>();
+                Assert.Equal(2, optional.OrDefault(2));
+            }
+        }
+
+        public sealed class WhenGettingValueOrDefaultValueFromADelegate
+        {
+            [Fact]
+            public void WithValueItReturnsTheValue()
+            {
+                var optional = new Optional<int>(1);
+                Assert.Equal(1, optional.OrDefault(() => 2));
+            }
+
+            [Fact]
+            public void WithValueItDoesNotInvokeTheDelegate()
+            {
+                var theDelegate = A.Fake<Func<int>>();
+                var optional = new Optional<int>(1);
+                optional.OrDefault(theDelegate);
+                A.CallTo(theDelegate).MustNotHaveHappened();
+            }
+
+            [Fact]
+            public void WithoutValueItInvokesTheDelegateAndReturnsTheResult()
+            {
+                var optional = new Optional<int>();
+                Assert.Equal(2, optional.OrDefault(2));
+            }
+        }
+
+        public sealed class WhenEqualityIsDeterminedViaStronglyTypedOverload
+        {
+            [Fact]
+            public void WhenBothValuesAreMissingTheOptionalValuesAreEqual()
+            {
+                Assert.True(Optional<int>.None.Equals(Optional<int>.None));
+            }
+
+            [Fact]
+            public void WhenOnlyTheFirstValueIsMissingTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(Optional<int>.None.Equals(new Optional<int>(1)));
+            }
+
+            [Fact]
+            public void WhenOnlyTheSecondValueIsMissingTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int>(1).Equals(Optional<int>.None));
+            }
+
+            [Fact]
+            public void WhenRepresentedValuesAreEqualTheOptionalValuesAreEqual()
+            {
+                Assert.True(new Optional<int>(1).Equals(new Optional<int>(1)));
+            }
+
+            [Fact]
+            public void WhenRepresentedValuesAreNotEqualTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int>(1).Equals(new Optional<int>(2)));
+            }
+
+            [Fact]
+            public void WhenBothValuesAreNullTheOptionalValuesAreEqual()
+            {
+                Assert.True(new Optional<string>(null).Equals(new Optional<string>(null)));
+            }
+
+            [Fact]
+            public void WhenOnlyTheFirstValueIsNullTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<string>(null).Equals(new Optional<string>("")));
+            }
+
+            [Fact]
+            public void WhenOnlyTheSecondValueIsNullTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<string>("").Equals(new Optional<string>(null)));
+            }
+
+            [Fact]
+            public void WhenBothValuesAreEmptyNullablesTheOptionalValuesAreEqual()
+            {
+                Assert.True(new Optional<int?>(default(int?)).Equals(new Optional<int?>(default(int?))));
+            }
+
+            [Fact]
+            public void WhenOnlyTheFirstValueIsEmptyNullableTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int?>(default(int?)).Equals(new Optional<int?>(0)));
+            }
+
+            [Fact]
+            public void WhenOnlyTheSecondValueIsEmptyNullableTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int?>(0).Equals(new Optional<int?>(default(int?))));
+            }
+        }
+
+        public sealed class WhenEqualityIsDeterminedViaNonTypedOverload
+        {
+            [Fact]
+            public void WhenBothValuesAreMissingTheOptionalValuesAreEqual()
+            {
+                Assert.True(Optional<int>.None.Equals((object)Optional<int>.None));
+            }
+
+            [Fact]
+            public void WhenOnlyTheFirstValueIsMissingTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(Optional<int>.None.Equals((object)new Optional<int>(1)));
+            }
+
+            [Fact]
+            public void WhenOnlyTheSecondValueIsMissingTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int>(1).Equals((object)Optional<int>.None));
+            }
+
+            [Fact]
+            public void WhenRepresentedValuesAreEqualTheOptionalValuesAreEqual()
+            {
+                Assert.True(new Optional<int>(1).Equals((object)new Optional<int>(1)));
+            }
+
+            [Fact]
+            public void WhenRepresentedValuesAreNotEqualTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int>(1).Equals((object)new Optional<int>(2)));
+            }
+
+            [Fact]
+            public void WhenBothValuesAreNullTheOptionalValuesAreEqual()
+            {
+                Assert.True(new Optional<string>(null).Equals((object)new Optional<string>(null)));
+            }
+
+            [Fact]
+            public void WhenOnlyTheFirstValueIsNullTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<string>(null).Equals((object)new Optional<string>("")));
+            }
+
+            [Fact]
+            public void WhenOnlyTheSecondValueIsNullTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<string>("").Equals((object)new Optional<string>(null)));
+            }
+
+            [Fact]
+            public void WhenBothValuesAreEmptyNullablesTheOptionalValuesAreEqual()
+            {
+                Assert.True(new Optional<int?>(default(int?)).Equals((object)new Optional<int?>(default(int?))));
+            }
+
+            [Fact]
+            public void WhenOnlyTheFirstValueIsEmptyNullableTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int?>(default(int?)).Equals((object)new Optional<int?>(0)));
+            }
+
+            [Fact]
+            public void WhenOnlyTheSecondValueIsEmptyNullableTheOptionalValuesAreNotEqual()
+            {
+                Assert.False(new Optional<int?>(0).Equals((object)new Optional<int?>(default(int?))));
+            }
+
+            [Fact]
+            public void WhenTheArgumentIsNotAnOptionalValueTheObjectsAreNotEqual()
+            {
+                Assert.False(new Optional<int>(1).Equals((object)1));
+            }
+
+            [Fact]
+            public void WhenTheArgumentIsNullTheObjectsAreNotEqual()
+            {
+                Assert.False(new Optional<int>(1).Equals((object)null));
+            }
+
+            [Fact]
+            public void WhenTheArgumentIsAnOptionalValueOfADifferentTypeTheObjectsAreNotEqual()
+            {
+                Assert.False(new Optional<int>(1).Equals((object)new Optional<uint>(1)));
+            }
+        }
+
+        [Fact]
+        public void WithoutValueHashCodeIsStable()
+        {
+            var hashCode1 = Optional<int>.None.GetHashCode();
+            var hashCode2 = Optional<int>.None.GetHashCode();
+            Assert.True(hashCode1 == hashCode2);
+        }
+
+        [Fact]
+        public void WithValueHashCodeIsStable()
+        {
+            var hashCode1 = new Optional<int>(1).GetHashCode();
+            var hashCode2 = new Optional<int>(1).GetHashCode();
+            Assert.True(hashCode1 == hashCode2);
+        }
+
+        [Fact]
+        public void WithNullValueHashCodeIsStable()
+        {
+            var hashCode1 = new Optional<string>(null).GetHashCode();
+            var hashCode2 = new Optional<string>(null).GetHashCode();
+            Assert.True(hashCode1 == hashCode2);
+        }
+
+        [Fact]
+        public void WithEmptyNullableValueHashCodeIsStable()
+        {
+            var hashCode1 = new Optional<int?>(default(int?)).GetHashCode();
+            var hashCode2 = new Optional<int?>(default(int?)).GetHashCode();
+            Assert.True(hashCode1 == hashCode2);
+        }
+    }
+}
