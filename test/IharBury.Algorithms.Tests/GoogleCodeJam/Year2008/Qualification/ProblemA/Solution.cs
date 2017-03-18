46b0753ae58c58464f0394a69eb556b5538221f6
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-18 21:10:49 +0100

    Google Code Jam 2008 Qualification Problem A solution added

---
+++ test/IharBury.Algorithms.Tests/GoogleCodeJam/Year2008/Qualification/ProblemA/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using static System.FormattableString;
+using static System.Globalization.CultureInfo;
+using static System.Linq.Enumerable;
+
+namespace IharBury.Algorithms.Tests.GoogleCodeJam.Year2008.Qualification.ProblemA
+{
+    public static class Solution
+    {
+        public static int Solve(IReadOnlyCollection<string> searchEngines, IReadOnlyCollection<string> queries)
+        {
+            var queryGroupCount = queries
+                .GroupWhile(
+                    item => new HashSet<string>() { item },
+                    (group, item) =>
+                    {
+                        group.Add(item);
+                        return group;
+                    },
+                    (group, item) => group.Contains(item) || (group.Count + 1 < searchEngines.Count))
+                .Count();
+            return queryGroupCount == 0 ? 0 : queryGroupCount - 1;
+        }
+
+        public static void SolveDataset(TextReader input, TextWriter output)
+        {
+            var caseCount = int.Parse(input.ReadLine(), InvariantCulture);
+
+            for (var caseNumber = 1; caseNumber <= caseCount; caseNumber++)
+            {
+                var searchEngineCount = int.Parse(input.ReadLine(), InvariantCulture);
+                var searchEngines = Range(1, searchEngineCount).Select(_ => input.ReadLine()).ToList();
+                var queryCount = int.Parse(input.ReadLine(), InvariantCulture);
+                var queries = Range(1, queryCount).Select(_ => input.ReadLine()).ToList();
+
+                var switchCount = Solve(searchEngines, queries);
+
+                output.WriteLine(Invariant($"Case #{caseNumber}: {switchCount}"));
+            }
+        }
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/GoogleCodeJam/Year2008/Qualification/ProblemA/Tests.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using Xunit;
+
+using static IharBury.Algorithms.Tests.GoogleCodeJam.Year2008.Qualification.ProblemA.Solution;
+
+namespace IharBury.Algorithms.Tests.GoogleCodeJam.Year2008.Qualification.ProblemA
+{
+    public sealed class Tests
+    {
+        [Fact]
+        public void Sample1IsCorrect()
+        {
+            Assert.Equal(1, Solve(
+                new[]
+                {
+                    "Yeehaw",
+                    "NSM",
+                    "Dont Ask",
+                    "B9",
+                    "Googol"
+                },
+                new[]
+                {
+                    "Yeehaw",
+                    "Yeehaw",
+                    "Googol",
+                    "B9",
+                    "Googol",
+                    "NSM",
+                    "B9",
+                    "NSM",
+                    "Dont Ask",
+                    "Googol"
+                }));
+        }
+
+        [Fact]
+        public void Sample2IsCorrect()
+        {
+            Assert.Equal(0, Solve(
+                new[]
+                {
+                    "Yeehaw",
+                    "NSM",
+                    "Dont Ask",
+                    "B9",
+                    "Googol"
+                },
+                new[]
+                {
+                    "Googol",
+                    "Dont Ask",
+                    "NSM",
+                    "NSM",
+                    "Yeehaw",
+                    "Yeehaw",
+                    "Googol"
+                }));
+        }
+
+        [Fact]
+        public void SmallDatasetIsCorrect()
+        {
+            TestDataset("SmallInput.txt", "SmallOutput.txt");
+        }
+
+        [Fact]
+        public void LargeDatasetIsCorrect()
+        {
+            TestDataset("LargeInput.txt", "LargeOutput.txt");
+        }
+
+        private void TestDataset(string inputName, string outputName)
+        {
+            using (var inputStream = GetType().Assembly.GetManifestResourceStream(GetType(), inputName))
+            using (var input = new StreamReader(inputStream))
+            using (var outputStream = GetType().Assembly.GetManifestResourceStream(GetType(), outputName))
+            using (var output = new StreamReader(outputStream))
+            {
+                var actualOutput = new StringWriter();
+                SolveDataset(input, actualOutput);
+                Assert.Equal(output.ReadToEnd().TrimEnd('\r', '\n'), actualOutput.ToString().TrimEnd('\r', '\n'));
+            }
+        }
+    }
+}
