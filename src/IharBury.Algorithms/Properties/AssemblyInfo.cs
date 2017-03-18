0d8dcee771aaf0ee68efb302afcee3cabfe6006a
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-18 16:24:30 +0100

    wip v2

--- src/IharBury.Algorithms/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("IharBury.Algorithms")]
-[assembly: AssemblyProduct("IharBury.Algorithms")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.*")]
