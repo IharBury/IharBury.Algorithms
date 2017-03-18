0f82f045735be4029e6abfdf3712b6e78a9ea179
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-18 19:27:48 +0100

    IEnumerable.GroupWhile extension method added

---
+++ src/IharBury.Algorithms/EnumerableExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IharBury.Algorithms
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Combines all the items in <paramref name="items"/>
+        /// into sequential groups of custom type <typeparamref name="TGroup"/>.
+        /// The given <paramref name="shouldAddToGroup"/> delegate is used
+        /// to determine whether an item should be added to the existing group or to a new group.
+        /// </summary>
+        /// <typeparam name="TItem">Type of the items.</typeparam>
+        /// <typeparam name="TGroup">Type of the groups.</typeparam>
+        /// <param name="items">
+        /// The items to be grouped.
+        /// Cannot be <c>null</c>.
+        /// </param>
+        /// <param name="createGroup">
+        /// A delegate to create a new group with the given item in it.
+        /// Cannot be <c>null</c>.
+        /// </param>
+        /// <param name="addToGroup">
+        /// A delegate to add a new item to a group.
+        /// Returns the group with the new item added which can be same group object.
+        /// Can modify the old group object.
+        /// Cannot modify the item.
+        /// Cannot be <c>null</c>.
+        /// </param>
+        /// <param name="shouldAddToGroup">
+        /// The delegate to determine whether an item should be added to the existing group or to a new group.
+        /// Cannot modify the group.
+        /// Cannot modify the item.
+        /// Cannot be <c>null</c>.
+        /// </param>
+        public static IEnumerable<TGroup> GroupWhile<TItem, TGroup>(
+            this IEnumerable<TItem> items,
+            Func<TItem, TGroup> createGroup,
+            Func<TGroup, TItem, TGroup> addToGroup,
+            Func<TGroup, TItem, bool> shouldAddToGroup)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+            if (createGroup == null)
+                throw new ArgumentNullException(nameof(createGroup));
+            if (addToGroup == null)
+                throw new ArgumentNullException(nameof(addToGroup));
+            if (shouldAddToGroup == null)
+                throw new ArgumentNullException(nameof(shouldAddToGroup));
+
+            var group = Optional<TGroup>.None;
+
+            foreach (var item in items)
+            {
+                if (group.HasValue)
+                {
+                    if (shouldAddToGroup(group.Value, item))
+                    {
+                        group = addToGroup(group.Value, item);
+                    }
+                    else
+                    {
+                        yield return group.Value;
+                        group = createGroup(item);
+                    }
+                }
+                else
+                {
+                    group = createGroup(item);
+                }
+            }
+
+            if (group.HasValue)
+                yield return group.Value;
+        }
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace IharBury.Algorithms.Tests
+{
+    public sealed class EnumerableExtensionsTests
+    {
+        public sealed class WhenGroupingWhileAConditionIsHeld
+        {
+            [Fact]
+            public void WhenThereAreNoItemsThereAreNoGroups()
+            {
+                Assert.Empty(Enumerable.Empty<int>().GroupWhile(
+                    item => new List<int>() { item },
+                    (group, item) =>
+                    {
+                        group.Add(item);
+                        return group;
+                    },
+                    (group, item) => true));
+            }
+
+            [Fact]
+            public void WhenThereIsOnlyOneItemItFormsTheOnlyGroup()
+            {
+                var groups = new[] { 1 }
+                    .GroupWhile(
+                        item => new List<int>() { item },
+                        (group, item) =>
+                        {
+                            group.Add(item);
+                            return group;
+                        },
+                        (group, item) => true)
+                    .ToList();
+                Assert.Equal(1, groups.Count);
+                Assert.True(groups.Single().SequenceEqual(new[] { 1 }));
+            }
+
+            [Fact]
+            public void WhenItemsShouldBeInTheSameGroupTheyAre()
+            {
+                var groups = new[] { 1, 2, 3 }
+                    .GroupWhile(
+                        item => new List<int>() { item },
+                        (group, item) =>
+                        {
+                            group.Add(item);
+                            return group;
+                        },
+                        (group, item) => true)
+                    .ToList();
+                Assert.Equal(1, groups.Count);
+                Assert.True(groups.Single().SequenceEqual(new[] { 1, 2, 3 }));
+            }
+
+            [Fact]
+            public void WhenItemsShouldBeInDifferentGroupsTheyAre()
+            {
+                var groups = new[] { 1, 2, 3 }
+                    .GroupWhile(
+                        item => new List<int>() { item },
+                        (group, item) =>
+                        {
+                            group.Add(item);
+                            return group;
+                        },
+                        (group, item) => false)
+                    .ToList();
+                Assert.Equal(3, groups.Count);
+                Assert.True(groups[0].SequenceEqual(new[] { 1 }));
+                Assert.True(groups[1].SequenceEqual(new[] { 2 }));
+                Assert.True(groups[2].SequenceEqual(new[] { 3 }));
+            }
+
+            [Fact]
+            public void WhenAnItemShouldStartANewGroupItDoes()
+            {
+                var groups = new[] { 1, 2, 3, 4, 5 }
+                    .GroupWhile(
+                        item => new List<int>() { item },
+                        (group, item) =>
+                        {
+                            group.Add(item);
+                            return group;
+                        },
+                        (group, item) => item != 3)
+                    .ToList();
+                Assert.Equal(2, groups.Count);
+                Assert.True(groups[0].SequenceEqual(new[] { 1, 2 }));
+                Assert.True(groups[1].SequenceEqual(new[] { 3, 4, 5 }));
+            }
+
+            [Fact]
+            public void WhenAGroupShouldStopGrowingItDoes()
+            {
+                var groups = new[] { 1, 2, 3, 4, 5 }
+                    .GroupWhile(
+                        item => new List<int>() { item },
+                        (group, item) =>
+                        {
+                            group.Add(item);
+                            return group;
+                        },
+                        (group, item) => group.Count < 3)
+                    .ToList();
+                Assert.Equal(2, groups.Count);
+                Assert.True(groups[0].SequenceEqual(new[] { 1, 2, 3 }));
+                Assert.True(groups[1].SequenceEqual(new[] { 4, 5 }));
+            }
+
+            [Fact]
+            public void WhenAddingAnItemToAGroupProducesANewObjectItWorks()
+            {
+                var groups = new[] { 1, 2, 3 }
+                    .GroupWhile(
+                        item => new List<int>() { item },
+                        (group, item) =>
+                        {
+                            var newGroup = group.ToList();
+                            newGroup.Add(item);
+                            return newGroup;
+                        },
+                        (group, item) => true)
+                    .ToList();
+                Assert.Equal(1, groups.Count);
+                Assert.True(groups.Single().SequenceEqual(new[] { 1, 2, 3 }));
+            }
+        }
+    }
+}
