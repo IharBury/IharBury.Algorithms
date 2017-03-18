5f3f7d9117568e130c89fb5e5992f0252629819c
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-19 00:39:45 +0100

    Google Code Jam 2008 Qualification Problem B solution added

---
+++ test/IharBury.Algorithms.Tests/GoogleCodeJam/Year2008/Qualification/ProblemB/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using static System.FormattableString;
+using static System.Globalization.CultureInfo;
+using static System.Linq.Enumerable;
+
+namespace IharBury.Algorithms.Tests.GoogleCodeJam.Year2008.Qualification.ProblemB
+{
+    public static class Solution
+    {
+        public static (int trainCountAtA, int trainCountAtB) Solve(
+            TimeSpan turnaroundTime,
+            IReadOnlyCollection<(TimeSpan departureTime, TimeSpan arrivalTime)> tripsFromAToB,
+            IReadOnlyCollection<(TimeSpan departureTime, TimeSpan arrivalTime)> tripsFromBToA)
+        {
+            return (SolveOneWay(turnaroundTime, tripsFromAToB, tripsFromBToA),
+                SolveOneWay(turnaroundTime, tripsFromBToA, tripsFromAToB));
+        }
+
+        private static int SolveOneWay(
+            TimeSpan turnaroundTime,
+            IReadOnlyCollection<(TimeSpan departureTime, TimeSpan arrivalTime)> outboundTrips,
+            IReadOnlyCollection<(TimeSpan departureTime, TimeSpan arrivalTime)> inboundTrips)
+        {
+            var trainRequirementChanges = outboundTrips
+                .Select(trip => (time: trip.departureTime, changeAmount: 1))
+                .Concat(inboundTrips.Select(trip => (time: trip.arrivalTime + turnaroundTime, changeAmount: -1)));
+
+            return trainRequirementChanges
+                .OrderBy(change => change.time)
+                .ThenBy(change => change.changeAmount)
+                .GetRunningSumChecked(change => change.changeAmount)
+                .MaxOptional()
+                .OrDefault(0)
+                .ButMin(0);
+        }
+
+        public static void SolveDataset(TextReader input, TextWriter output)
+        {
+            (TimeSpan departure, TimeSpan arrival) ParseTrip(string trip)
+            {
+                var times = trip.Split(' ').Select(time => TimeSpan.Parse(time, InvariantCulture)).ToList();
+                return (times[0], times[1]);
+            }
+
+            var caseCount = int.Parse(input.ReadLine(), InvariantCulture);
+
+            for (var caseNumber = 1; caseNumber <= caseCount; caseNumber++)
+            {
+                var turnaround = TimeSpan.FromMinutes(int.Parse(input.ReadLine(), InvariantCulture));
+
+                var tripCounts = input.ReadLine().Split(' ').Select(count => int.Parse(count, InvariantCulture)).ToList();
+                var tripCountFromAToB = tripCounts[0];
+                var tripCountFromBToA = tripCounts[1];
+
+                var tripsFromAToB = Range(1, tripCountFromAToB).Select(_ => ParseTrip(input.ReadLine())).ToList();
+                var tripsFromBToA = Range(1, tripCountFromBToA).Select(_ => ParseTrip(input.ReadLine())).ToList();
+
+                (var trainCountAtA, var trainCountAtB) = Solve(turnaround, tripsFromAToB, tripsFromBToA);
+
+                output.WriteLine(Invariant($"Case #{caseNumber}: {trainCountAtA} {trainCountAtB}"));
+            }
+        }
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/GoogleCodeJam/Year2008/Qualification/ProblemB/Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+using static IharBury.Algorithms.Tests.GoogleCodeJam.Year2008.Qualification.ProblemB.Solution;
+
+namespace IharBury.Algorithms.Tests.GoogleCodeJam.Year2008.Qualification.ProblemB
+{
+    public sealed class Tests
+    {
+        [Fact]
+        public void Sample1IsCorrect()
+        {
+            (var trainCountAtA, var trainCountAtB) = Solve(
+                TimeSpan.FromMinutes(5),
+                new[]
+                {
+                    (new TimeSpan(9, 0, 0), new TimeSpan(12, 0, 0)),
+                    (new TimeSpan(10, 0, 0), new TimeSpan(13, 0, 0)),
+                    (new TimeSpan(11, 0, 0), new TimeSpan(12, 30, 0))
+                },
+                new[]
+                {
+                    (new TimeSpan(12, 2, 0), new TimeSpan(15, 0, 0)),
+                    (new TimeSpan(9, 0, 0), new TimeSpan(10, 30, 0))
+                });
+
+            Assert.Equal(2, trainCountAtA);
+            Assert.Equal(2, trainCountAtB);
+        }
+
+        public void Sample2IsCorrect()
+        {
+            (var trainCountAtA, var trainCountAtB) = Solve(
+                TimeSpan.FromMinutes(2),
+                new[]
+                {
+                    (new TimeSpan(9, 0, 0), new TimeSpan(9, 1, 0)),
+                    (new TimeSpan(12, 0, 0), new TimeSpan(12, 2, 0))
+                },
+                new ValueTuple<TimeSpan, TimeSpan>[] { });
+
+            Assert.Equal(2, trainCountAtA);
+            Assert.Equal(0, trainCountAtB);
+        }
+
+        [Fact]
+        public void SmallDatasetIsCorrect()
+        {
+            TestDataset("SmallInput.txt", "SmallOutput.txt");
+        }
+
+        [Fact]
+        public void LargeDatasetIsCorrect()
+        {
+            TestDataset("LargeInput.txt", "LargeOutput.txt");
+        }
+
+        private void TestDataset(string inputName, string outputName)
+        {
+            using (var inputStream = GetType().Assembly.GetManifestResourceStream(GetType(), inputName))
+            using (var input = new StreamReader(inputStream))
+            using (var outputStream = GetType().Assembly.GetManifestResourceStream(GetType(), outputName))
+            using (var output = new StreamReader(outputStream))
+            {
+                var actualOutput = new StringWriter();
+                SolveDataset(input, actualOutput);
+                Assert.Equal(output.ReadToEnd().TrimEnd('\r', '\n'), actualOutput.ToString().TrimEnd('\r', '\n'));
+            }
+        }
+    }
+}
