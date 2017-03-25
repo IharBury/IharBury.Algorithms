9abca2344f78dd3bcc9ba4dea8fad118ba16dc3a
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-25 23:56:05 +0100

    Double precision approximate two dimensional triangle

---
+++ src/IharBury.Algorithms/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/Triangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using static System.Math;
+
+namespace IharBury.Algorithms.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision
+{
+    /// <summary>
+    /// Represents a triangle in a two-dimensional Euclidean space
+    /// by its vertices with approximate Cartesian coordinates                                                      
+    /// with <see cref="double"/> precision.
+    /// </summary>
+    public struct Triangle
+    {
+        /// <summary>
+        /// Constructs a new triangle from its vertices.
+        /// </summary>
+        public Triangle(Point baseVertex, Point clockwiseVertex, Point anticlockwiseVertex)
+        {
+            BaseVertex = baseVertex;
+            ClockwiseVertex = clockwiseVertex;
+            AnticlockwiseVertex = anticlockwiseVertex;
+        }
+
+        /// <summary>
+        /// One of the vertices of the triangle.
+        /// </summary>
+        public Point BaseVertex { get; }
+
+        /// <summary>
+        /// The next vertex clockwise after <see cref="BaseVertex"/>.
+        /// </summary>
+        public Point ClockwiseVertex { get; }
+
+        /// <summary>
+        /// The next vertex anticlockwise after <see cref="BaseVertex"/>.
+        /// </summary>
+        public Point AnticlockwiseVertex { get; }
+
+        public override string ToString() => $"({BaseVertex}, {ClockwiseVertex}, {AnticlockwiseVertex})";
+
+        /// <summary>
+        /// Approximate area of the triangle.
+        /// </summary>
+        public double Area
+        {
+            get
+            {
+                var clockwiseVectorX = ClockwiseVertex.X - BaseVertex.X;
+                var clockwiseVectorY = ClockwiseVertex.Y - BaseVertex.Y;
+                var anticlockwiseVectorX = AnticlockwiseVertex.X - BaseVertex.X;
+                var anticlockwiseVectorY = AnticlockwiseVertex.Y - BaseVertex.Y;
+                return (clockwiseVectorY * anticlockwiseVectorX - clockwiseVectorX * anticlockwiseVectorY) / 2;
+            }
+        }
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/TriangleTests.cs
@@ -0,0 +1,50 @@
+﻿using IharBury.Algorithms.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision;
+using Xunit;
+
+namespace IharBury.Algorithms.Tests.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision
+{
+    public sealed class TriangleTests
+    {
+        [Fact]
+        public void ConstructedTriangleHasTheGivenBaseVertexX()
+        {
+            Assert.Equal(2, new Triangle(new Point(2, 3), new Point(4, 7), new Point(6, 1)).BaseVertex.X);
+        }
+
+        [Fact]
+        public void ConstructedTriangleHasTheGivenBaseVertexY()
+        {
+            Assert.Equal(3, new Triangle(new Point(2, 3), new Point(4, 7), new Point(6, 1)).BaseVertex.Y);
+        }
+
+        [Fact]
+        public void ConstructedTriangleHasTheGivenClockwiseVertexX()
+        {
+            Assert.Equal(4, new Triangle(new Point(2, 3), new Point(4, 7), new Point(6, 1)).ClockwiseVertex.X);
+        }
+
+        [Fact]
+        public void ConstructedTriangleHasTheGivenClockwiseVertexY()
+        {
+            Assert.Equal(7, new Triangle(new Point(2, 3), new Point(4, 7), new Point(6, 1)).ClockwiseVertex.Y);
+        }
+
+        [Fact]
+        public void ConstructedTriangleHasTheGivenAnticlockwiseVertexX()
+        {
+            Assert.Equal(6, new Triangle(new Point(2, 3), new Point(4, 7), new Point(6, 1)).AnticlockwiseVertex.X);
+        }
+
+        [Fact]
+        public void ConstructedTriangleHasTheGivenAnticlockwiseVertexY()
+        {
+            Assert.Equal(1, new Triangle(new Point(2, 3), new Point(4, 7), new Point(6, 1)).AnticlockwiseVertex.Y);
+        }
+
+        [Fact]
+        public void TriangleAreaIsCalculatedCorrectly()
+        {
+            Assert.Equal(10, new Triangle(new Point(2, 3), new Point(4, 7), new Point(6, 1)).Area, 5);
+        }
+    }
+}
