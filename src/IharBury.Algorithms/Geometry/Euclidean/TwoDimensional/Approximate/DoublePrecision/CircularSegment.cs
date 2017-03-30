5430f8803bdf91e1d0ed6749d4706133bf049d59
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-30 17:58:27 +0200

    Double precision approximate two dimensional circular segment

---
+++ src/IharBury.Algorithms/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/CircularSegment.cs
@@ -0,0 +1,65 @@
+﻿using static System.Math;
+
+namespace IharBury.Algorithms.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision
+{
+    /// <summary>
+    /// Represents a circular segment in a two-dimensional Euclidean space
+    /// by the center of its circle, its clockwise vertex with approximate Cartesian coordinates
+    /// and its approximate central angle with <see cref="double"/> precision.
+    /// </summary>
+    public struct CircularSegment
+    {
+        /// <summary>
+        /// Constructs a new circular segment from its circle center, clockwise vertex and central angle.
+        /// </summary>
+        public CircularSegment(Point circleCenter, Point clockwiseVertex, AngleMeasure centralAngle)
+        {
+            CircleCenter = circleCenter;
+            ClockwiseVertex = clockwiseVertex;
+            CentralAngle = centralAngle;
+        }
+
+        /// <summary>
+        /// The center of the circle.
+        /// </summary>
+        public Point CircleCenter { get; }
+
+        /// <summary>
+        /// The vertex clockwise from the bounding chord.
+        /// </summary>
+        public Point ClockwiseVertex { get; }
+
+        /// <summary>
+        /// The central angle.
+        /// </summary>
+        public AngleMeasure CentralAngle { get; }
+
+        public override string ToString() => $"({CircleCenter}, {ClockwiseVertex}, {CentralAngle})";
+
+        /// <summary>
+        /// The radius line segment between <see cref="CircleCenter"/> and <see cref="ClockwiseVertex"/>.
+        /// </summary>
+        public LineSegment ClockwiseRadius => new LineSegment(CircleCenter, ClockwiseVertex);
+
+        /// <summary>
+        /// The approximate squared length of the radius of the circle.
+        /// </summary>
+        public double SquaredRadiusLength => ClockwiseRadius.SquaredLength;
+
+        /// <summary>
+        /// The approximate length of the radius of the circle.
+        /// Less optimal than <see cref="SquaredRadiusLength"/>.
+        /// </summary>
+        public double RadiusLength => Sqrt(SquaredRadiusLength);
+
+        /// <summary>
+        /// The circle of the circular segment.
+        /// </summary>
+        public Circle Circle => new Circle(CircleCenter, RadiusLength);
+
+        /// <summary>
+        /// The approximate area of the circular segment.
+        /// </summary>
+        public double Area => (CentralAngle.InRadians - Sin(CentralAngle.InRadians)) / 2 * SquaredRadiusLength;
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/CircularSegmentTests.cs
@@ -0,0 +1,80 @@
+﻿using IharBury.Algorithms.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision;
+using Xunit;
+
+namespace IharBury.Algorithms.Tests.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision
+{
+    public sealed class CircularSegmentTests
+    {
+        [Fact]
+        public void ConstructedCircluarSegmentHasTheGivenCircleCenterX()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(4.34, circularSegment.CircleCenter.X);
+        }
+
+        [Fact]
+        public void ConstructedCircluarSegmentHasTheGivenCircleCenterY()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(6.12, circularSegment.CircleCenter.Y);
+        }
+
+        [Fact]
+        public void ConstructedCircluarSegmentHasTheGivenClockwiseVertexX()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(5.1, circularSegment.ClockwiseVertex.X);
+        }
+
+        [Fact]
+        public void ConstructedCircluarSegmentHasTheGivenClockwiseVertexY()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(6.22, circularSegment.ClockwiseVertex.Y);
+        }
+
+        [Fact]
+        public void ConstructedCircluarSegmentHasTheGivenCentralAngleInRadians()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(0.14, circularSegment.CentralAngle.InRadians);
+        }
+
+        [Fact]
+        public void ClockwiseRadiusIsCalculatedCorrectly()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            var expectedClockwiseRadius = new LineSegment(new Point(4.34, 6.12), new Point(5.1, 6.22));
+            Assert.True(circularSegment.ClockwiseRadius.HasSameCoordinatesAs(expectedClockwiseRadius, 0.00001));
+        }
+
+        [Fact]
+        public void SquaredRadiusLengthIsCalculatedCorrectly()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(0.5876, circularSegment.SquaredRadiusLength, 5);
+        }
+
+        [Fact]
+        public void RadiusLengthIsCalculatedCorrectly()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(0.76655071586947200502188458103729, circularSegment.RadiusLength, 5);
+        }
+
+        [Fact]
+        public void CircleIsCalculatedCorrectly()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            var expectedCircle = new Circle(new Point(4.34, 6.12), 0.76655071586947200502188458103729);
+            Assert.True(circularSegment.Circle.HasSameCoordinatesAs(expectedCircle, 0.00001, 0.00001));
+        }
+
+        [Fact]
+        public void AreaIsCalculatedCorrectly()
+        {
+            var circularSegment = new CircularSegment(new Point(4.34, 6.12), new Point(5.1, 6.22), new AngleMeasure(0.14));
+            Assert.Equal(1.3423291752332167125502226896182e-4, circularSegment.Area, 5);
+        }
+    }
+}
