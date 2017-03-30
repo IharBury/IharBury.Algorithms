761539f1659e6b82c2b2c44faa36aac2886e843d
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-30 18:29:59 +0200

    Double precision approximate two dimensional directed line

---
+++ src/IharBury.Algorithms/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/DirectedLine.cs
@@ -0,0 +1,27 @@
+﻿namespace IharBury.Algorithms.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision
+{
+    /// <summary>
+    /// Represents a directed line in a two-dimensional Euclidean space
+    /// by two of its points with approximate Cartesian coordinates with <see cref="double"/> precision.
+    /// </summary>
+    public struct DirectedLine
+    {
+        public DirectedLine(Point basePoint, Point forwardPoint)
+        {
+            BasePoint = basePoint;
+            ForwardPoint = forwardPoint;
+        }
+
+        /// <summary>
+        /// One of the points of the directed line.
+        /// </summary>
+        public Point BasePoint { get; }
+
+        /// <summary>
+        /// One of the points of the directed line which is forward from <see cref="BasePoint"/>.
+        /// </summary>
+        public Point ForwardPoint { get; }
+
+        public override string ToString() => $"({BasePoint}, {ForwardPoint})";
+    }
+}
---
+++ test/IharBury.Algorithms.Tests/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/DirectedLineTests.cs
@@ -0,0 +1,32 @@
+﻿using IharBury.Algorithms.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision;
+using Xunit;
+
+namespace IharBury.Algorithms.Tests.Geometry.Euclidean.TwoDimensional.Approximate.DoublePrecision
+{
+    public sealed class DirectedLineTests
+    {
+        [Fact]
+        public void ConstructedDirectedLineHasTheGivenBasePointX()
+        {
+            Assert.Equal(2, new DirectedLine(new Point(2, 3), new Point(4, 7)).BasePoint.X);
+        }
+
+        [Fact]
+        public void ConstructedDirectedLineHasTheGivenBasePointY()
+        {
+            Assert.Equal(3, new DirectedLine(new Point(2, 3), new Point(4, 7)).BasePoint.Y);
+        }
+
+        [Fact]
+        public void ConstructedDirectedLineHasTheGivenForwardPointX()
+        {
+            Assert.Equal(4, new DirectedLine(new Point(2, 3), new Point(4, 7)).ForwardPoint.X);
+        }
+
+        [Fact]
+        public void ConstructedDirectedLineHasTheGivenForwardPointY()
+        {
+            Assert.Equal(7, new DirectedLine(new Point(2, 3), new Point(4, 7)).ForwardPoint.Y);
+        }
+    }
+}
