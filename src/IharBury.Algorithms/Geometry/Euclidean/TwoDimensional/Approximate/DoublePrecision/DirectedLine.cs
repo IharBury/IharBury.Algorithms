cb50661ed4df00ed82606907da93caf64c54aaa2
Ihar Bury <Ihar.Bury@gmail.com>
2017-03-30 18:48:33 +0200

    wip DirectedLine.HasToTheLeft

--- src/IharBury.Algorithms/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/DirectedLine.cs
+++ src/IharBury.Algorithms/Geometry/Euclidean/TwoDimensional/Approximate/DoublePrecision/DirectedLine.cs
@@ -23,5 +23,21 @@
         public Point ForwardPoint { get; }
 
         public override string ToString() => $"({BasePoint}, {ForwardPoint})";
+
+        /// <summary>
+        /// Determines whether the given point is to the left with the given max squared distance error.
+        /// </summary>
+        public bool HasToTheLeft(Point point, double maxSquaredDistanceError)
+        {
+            var xDifference1 = BasePoint.X - point.X;
+            var yDifference1 = BasePoint.Y - ForwardPoint.Y;
+            var xDifference2 = BasePoint.X - ForwardPoint.X;
+            var yDifference2 = BasePoint.Y - point.Y;
+            var areaFactor = xDifference1 * yDifference1 - xDifference2 * yDifference2;
+            if (areaFactor > 0)
+                return true;
+            var squaredDistance = areaFactor * areaFactor / BasePoint.GetSquaredDistanceTo(ForwardPoint);
+            return squaredDistance <= maxSquaredDistanceError;
+        }
     }
 }
